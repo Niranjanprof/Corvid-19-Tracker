3ccea8802ea890d763cc0ec457867f49388cecd3
Niranjan B <niranjannb7777@gmail.com>
2020-04-28 16:29:27 +0530

    inifinite trial updated

--- CovidTracker1/jsontodict.py
+++ CovidTracker1/jsontodict.py
@@ -1,10 +1,14 @@
 import requests
+import time
 
-try:
-    # Collects Data from the api
-    response = requests.get('https://coronavirus-tracker-api.herokuapp.com/v2/locations?timelines=true')
-except:
-    print('Network Error: Check your Connection')
-    exit()
+while True: 
+    try:
+        # Collects Data from the api
+        response = requests.get('https://coronavirus-tracker-api.herokuapp.com/v2/locations?timelines=true')
+    except:
+        print("Network down.")
+        time.sleep(1)
+    else:
+        break
 # Converts to python dictionary
 data = response.json()
