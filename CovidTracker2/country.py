e8991e64ff96d08a3784faa5d31d7b5bf4d06f33
Niranjan B <niranjannb7777@gmail.com>
2020-04-29 17:03:32 +0530

    Country Updates

--- CovidTracker2/country.py
+++ CovidTracker2/country.py
@@ -3,14 +3,19 @@ import timeline
 import matplotlib.pyplot as plt
 import url
 
+
 # Collects Query from User
 print("Country Specific Data....................")
 country = input("Enter the Country: ").strip()
 
-# Collects Data
+
 request_url = url.url + 'total/country/' + country
 info = requests.get(request_url)
-data = info.json()[-1]
+try:
+    data = info.json()[-1]
+except:
+    print("No data Available")
+    quit()
 
 coordinates = dict(lat=data['Lat'], lon=data['Lon'])
 confirmed = data['Confirmed']
@@ -35,7 +40,7 @@ slices=[confirmed,deaths,recovered]
 
 activities=['Confirmed Cases','No. of Deaths','Recovered Cases']
 color=['g','r','tab:orange']
-plt.pie(slices,labels=activities,colors=color,startangle=90,shadow=True,explode=(0,0,0.2),autopct='%1.2f%%')
+plt.pie(slices,labels=activities,colors=color,startangle=45,shadow=True,explode=(0,0,0.2),autopct='%1.2f%%')
 plt.title(country)
 plt.legend(loc='best', bbox_to_anchor=(0.8, 0, 0.3, 0.2))
 plt.show()
